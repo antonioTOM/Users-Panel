5ae848111412f0bc436b7f836c229fa9741b1532
antonioTOM <antoniotomassini@cambiserrani.it>
2022-08-10 23:48:22 +0200

    commited updates to index.html & script.js

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ const user2 = new User (usersName[0].innerText, "LindieH67", "Hey I'm Linda and 
 user2.isGenderMorF(2);
 console.log(user2);
 
-const user3 = new User (usersName[1].innerText, "M4DROG3R008", "Hi I'm 25 and I'm a highschool math teacher", "anttom0713@gmail.com", "327 630 3660", "via Isonzo, 130", "13/07/2007");
-user1.isGenderMorF(1);
+const user3 = new User (usersName[1].innerText, "M4DROG3R008", "Hi I'm 30 and I'm a high school math teacher", "rogermed1na@abcdhighschool.ef", "76252492457546564134898", usersAddress[1].innerText, "07/10/1992");
+user3.isGenderMorF(1);
 console.log(user3);
+
+const user4 = new User (usersName[2].innerText, "It'sDeanGuys", "Hi I'm Nigerian and I'm currently studying Web Development at MIT", "sullyd3an96@abcd.ef", "632786358547889654893163186", usersAddress[2].innerText, "19/02/1996");
+user4.isGenderMorF(1);
+console.log(user4);
