07c84406e263adb84f83487cda791673f8610463
antonioTOM <antoniotomassini@cambiserrani.it>
2022-08-21 20:58:11 +0200

    Modified script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const usersImage = document.querySelectorAll("img").getAttribute("src");
 const usersName = document.querySelectorAll("h4");
 const usersAddress = document.querySelectorAll("p");
 
 class User {
-  constructor(name, username, bio, email, phoneNumber, address, birthDate, gender) {
+  constructor(image, name, username, bio, email, phoneNumber, address, birthDate, gender) {
+    this.image = image;
     this.name = name;
     this.username = username;
     this.bio = bio;
@@ -28,15 +30,15 @@ const testUser = new User ("Anto", "anttom07", "Hi me is me, I'm Italian and I'm
 testUser.isGenderMorF(1);
 console.log(testUser);
 
-const user1 = new User (usersName[0].innerText, "LindieH67", "Hey I'm Linda and I'm a painter", "lindahart6767@abcd.ef", "757885336788543789", usersAddress[0].innerText, "27/05/1990");
+const user1 = new User (usersImage[0], usersName[0].innerText, "LindieH67", "Hey I'm Linda and I'm a painter", "lindahart6767@abcd.ef", "757885336788543789", usersAddress[0].innerText, "27/05/1990");
 user1.isGenderMorF(2);
 console.log(user1);
 
-const user2 = new User (usersName[1].innerText, "M4DROG3R008", "Hi I'm 30 and I'm a high school math teacher", "rogermed1na@abcdhighschool.ef", "76252492457546564134898", usersAddress[1].innerText, "07/10/1992");
+const user2 = new User (usersImage[1], usersName[1].innerText, "M4DROG3R008", "Hi I'm 30 and I'm a high school math teacher", "rogermed1na@abcdhighschool.ef", "76252492457546564134898", usersAddress[1].innerText, "07/10/1992");
 user2.isGenderMorF(1);
 console.log(user2);
 
-const user3 = new User (usersName[2].innerText, "It'sDeanGuys", "Hi I'm Nigerian and I'm currently studying Web Development on MIMO", "sullyd3an96@abcd.ef", "632786358547889654893163186", usersAddress[2].innerText, "19/02/1996");
+const user3 = new User (usersImage[2], usersName[2].innerText, "It'sDeanGuys", "Hi I'm Nigerian and I'm currently studying Web Development on MIMO", "sullyd3an96@abcd.ef", "632786358547889654893163186", usersAddress[2].innerText, "19/02/1996");
 user3.isGenderMorF(1);
 console.log(user3);
 
